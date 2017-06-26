7379476c169d6cb60cf1e6b5b082bfdf8dd7b596
Dimitris Lyrintzis <luminedward@gmail.com>
2017-06-26 12:57:03 +0300

    changed label to signify custom or default donation
    also modified category to reflect plastics cdf donation

--- google-tag-manager/plastics-cdf/track_donation_completed.js
+++ google-tag-manager/plastics-cdf/track_donation_completed.js
@@ -2,14 +2,15 @@ if($("#successBox").hasClass("show")){
   console.log("donation button clicked");
 
   var giftAmountTracker = $("#giftAmountTracker").val();
-  var paymentMethod = ""; // TODO: Implement getting the payment method.
+  var isDefaultAmount = True // False if the supporterd has entered a custom amount.
+  var donationLabel = ((isDefaultAmount) ? "Default Amount" : "Custom Amount");
 
   window.dataLayer.push({
     'plasticsCdfDonation' : {
       'value': giftAmountTracker,
       'action': "Donated With Credit Card",
-      'category': 'Once-Off Donation',
-      'label' : 'Plastics Crowdfunding'
+      'category': 'Plastics CDF Donation',
+      'label' : donationLabel,
     },
     'event' : 'plasticsCdfDonationCompleted'
   });
