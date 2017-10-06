ce691d557fd5a9d830b3ce0948135e9d09ef3c90
edLum <dliritzi@greenpeace.org>
2017-10-06 17:41:31 +0300

    Created calendar datalayer pushes

---
+++ google-tag-manager/sea-box/calendar/add_box.js
@@ -0,0 +1,11 @@
+//  to be fired when a box is added to a date. 
+
+window.dataLayer.push{
+  'pick_date': {
+    'category': 'calendar',
+    'action': 'add-box',
+    'label': 'fish-box',
+  },
+  'event': 'boxAdded'
+});
+
---
+++ google-tag-manager/sea-box/calendar/pick-date.js
@@ -0,0 +1,11 @@
+//  to be fired when a date is picked in
+
+window.dataLayer.push{
+  'pick_date': {
+    'category': 'calendar',
+    'action': 'pick-date',
+    'label': 'fish-box',
+  },
+  'event': 'datePicked'
+});
+
