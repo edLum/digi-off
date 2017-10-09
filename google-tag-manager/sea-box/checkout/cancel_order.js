32441da89e24a210c8e528bfe6287e84107bd04f
edLum <dliritzi@greenpeace.org>
2017-10-09 11:38:11 +0300

    dataLayer pushes for checkout page added

---
+++ google-tag-manager/sea-box/checkout/cancel_order.js
@@ -0,0 +1,9 @@
+//  to be fired when an order is cancelled in the checkout page.
+
+window.dataLayer.push{
+  'category': 'checkout',
+  'action': 'cancel-order', 
+  'label': 'fish-box',
+  'event': 'checkout-cancel-order'
+});
+
---
+++ google-tag-manager/sea-box/checkout/place_order.js
@@ -0,0 +1,9 @@
+//  to be fired when an order is placed in the checkout page.
+
+window.dataLayer.push{
+  'category': 'checkout',
+  'action': 'place-order', 
+  'label': 'fish-box',
+  'event': 'checkout-place-order'
+});
+
